05c91bf234b7aa8324db06bad13073cc9949fb67
CypherO2 <charlespresley2006@outlook.com>
2025-11-11 23:27:00 +0000

    add: project setup (3)

--- backend/app.py
+++ backend/app.py
@@ -5,8 +5,20 @@ from fastapi import FastAPI
 import uvicorn
 from core.config.settings import Settings
 from core.config.logger import logger
+from routes import router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title=Settings.APP_NAME, version=Settings.APP_VERSION, description=Settings.APP_DESCRIPTION)
+
+app.include_router(router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[Settings.FRONTEND_URL],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.on_event("startup")
 async def startup_event():
@@ -21,6 +33,3 @@ async def shutdown_event():
     Shutdown event for the application.
     """
     logger.info("shutdown event | GoldGrapher Backend is shutting down (version: %s)", Settings.APP_VERSION)
-
-if __name__ == "__main__":
-    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True) 
